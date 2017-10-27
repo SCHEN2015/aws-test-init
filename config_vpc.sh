c03d404293e5e0b42db82c8fff5bd3b4ab1786ae
Charles Shih <shichen120@gmail.com>
2017-10-27 10:38:59 +0800

    handle igw

--- config_vpc.sh
+++ config_vpc.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+# Resource should be created:
+# vpc-12345678 | cheshi_vpc_perf
+# igw-12345678 | cheshi_igw_perf
+# subnet-12345678 | cheshi_subnet_perf
+# rtb-12345678 | cheshi_rtb_perf
+# sg-12345678 | cheshi_sg_perf
+
+
 function tag2id()
 {
     # Query resource by tag and show its resource-id
     [ -z "$1" ] && exit 1
     aws ec2 describe-tags --filters "Name=value,Values=$1" --output json | jq -r .Tags[].ResourceId
 }
+
 
 function create_vpc()
 {
@@ -28,14 +37,14 @@ function create_vpc()
     # Create tag
     x=$(aws ec2 create-tags --resources $vpcid --tags Key=Name,Value=cheshi_vpc_perf --output json)
     if [ $? -eq 0 ]; then
-        echo "tag created for this vpc."
+        echo "tag created for this resource."
     else
         echo "$0: line $LINENO: \"aws ec2 create-tags\" failed."
         exit 1
     fi
 
     # Enable DNS
-    x=$(aws ec2 modify-vpc-attribute --vpc-id $vpcid --enable-dns-hostnames --enable-dns-support)
+    x=$(aws ec2 modify-vpc-attribute --vpc-id $vpcid --enable-dns-hostnames --enable-dns-support --output json)
     if [ $? -eq 0 ]; then
         echo "enabled dns for this vpc."
     else
@@ -46,8 +55,11 @@ function create_vpc()
     exit 0
 }
 
+
 function describe_vpc()
 {
+    [ -z "$1" ] && exit 1
+
     vpcid=$1
     aws ec2 describe-vpcs --vpc-id $vpcid --output table
     aws ec2 describe-vpc-attribute --vpc-id $vpcid --attribute enableDnsSupport --output table
@@ -57,10 +69,62 @@ function describe_vpc()
 }
 
 
+function create_igw()
+{
+    # Name tag: cheshi_igw_perf
+    # VPC: vpc-12345678 | cheshi_vpc_perf
+
+    # Create IGW
+    x=$(aws ec2 create-internet-gateway --output json)
+    if [ $? -eq 0 ]; then
+        igwid=$(echo $x | jq -r .InternetGateway.InternetGatewayId)
+        echo "new igw created, resource-id = $igwid."
+    else
+        echo "$0: line $LINENO: \"aws ec2 create-internet-gateway\" failed."
+        exit 1
+    fi
+
+    # Create tag
+    x=$(aws ec2 create-tags --resources $igwid --tags Key=Name,Value=cheshi_igw_perf --output json)
+    if [ $? -eq 0 ]; then
+        echo "tag created for this resource."
+    else
+        echo "$0: line $LINENO: \"aws ec2 create-tags\" failed."
+        exit 1
+    fi
+
+    # Attach to VPC
+    vpcid=$(tag2id cheshi_vpc_perf)
+    x=$(aws ec2 attach-internet-gateway --internet-gateway-id $igwid --vpc-id $vpcid --output json)
+    if [ $? -eq 0 ]; then
+        echo "attached igw to the vpc."
+    else
+        echo "$0: line $LINENO: \"aws ec2 attach-internet-gateway\" failed."
+        exit 1
+    fi
+
+    exit 0
+}
+
+
+function describe_igw()
+{
+    [ -z "$1" ] && exit 1
+
+    igwid=$1
+    aws ec2 describe-internet-gateways --internet-gateway-ids $igwid --output table
+
+    exit 0
+}
+
+
+
+
 function main()
 {
     date
-    describe_vpc $(tag2id cheshi_vpc_perf)
+    #describe_vpc $(tag2id cheshi_vpc_perf)
+    describe_igw $(tag2id cheshi_igw_perf)
 }
 
 main
